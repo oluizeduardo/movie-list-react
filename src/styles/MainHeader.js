ea2702ceb737df5f0323b5b8678ba6689f4385a9
Luiz Eduardo da Costa <luiz_costa@hst.br>
2022-09-13 22:55:15 -0300

    Refactoring.

--- src/styles/MainHeader.js
+++ src/styles/MainHeader.js
@@ -5,4 +5,6 @@ export const MainHeader = styled.header`
 	background: ${variables.primaryColor};
 	padding: 40px;
 	margin-bottom: 30px;
+	-moz-box-shadow: 0px 11px 10px 0px rgba(0,0,0,0.42);
+	box-shadow: 0px 11px 10px 0px rgba(0,0,0,0.42);
 `;
--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,3 +1,3 @@
 export const variables = {
-	primaryColor: "#eee",
+	primaryColor: "#B273F5",
 }
